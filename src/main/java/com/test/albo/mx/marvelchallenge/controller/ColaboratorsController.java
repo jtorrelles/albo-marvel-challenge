2f56d2954bbcd61620729db3637ed91e195200be
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-03 13:39:11 -0300

    se ajustan las clases segun los unti test

--- src/main/java/com/test/albo/mx/marvelchallenge/controller/ColaboratorsController.java
+++ src/main/java/com/test/albo/mx/marvelchallenge/controller/ColaboratorsController.java
@@ -1,23 +1,28 @@
 package com.test.albo.mx.marvelchallenge.controller;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.albo.mx.marvelchallenge.dto.ColaboratorsDto;
+import com.test.albo.mx.marvelchallenge.service.ColaboratorsService;
 
 @RestController
 @RequestMapping("colaborators")
 public class ColaboratorsController {
 
-	@GetMapping("/{name}")
-	public ResponseEntity<ColaboratorsDto> getColaborators() {
-		return new ResponseEntity<>(new ColaboratorsDto(LocalDateTime.now(), new ArrayList<String>(),
-				new ArrayList<String>(), new ArrayList<String>()), HttpStatus.OK);
+	private ColaboratorsService colaboratorService;
+
+	public ColaboratorsController(ColaboratorsService colaboratorService) {
+		this.colaboratorService = colaboratorService;
+	}
+
+	@GetMapping("/{character}")
+	public ResponseEntity<ColaboratorsDto> getColaborators(@PathVariable("character") String characterName) {
+		var colaborators = colaboratorService.getColaborators(characterName);
+		return new ResponseEntity<>(colaborators, HttpStatus.OK);
 	}
 }
--- src/main/java/com/test/albo/mx/marvelchallenge/dto/ColaboratorsDto.java
+++ src/main/java/com/test/albo/mx/marvelchallenge/dto/ColaboratorsDto.java
@@ -7,8 +7,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ColaboratorsDto {
 
