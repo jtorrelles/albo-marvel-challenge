4fdedaed4d5f68081ca564f3bbc7614b41b6f533
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-03 11:55:45 -0300

    project skeleton

---
+++ src/main/java/com/test/albo/mx/marvelchallenge/MarvelApiToAlboApplication.java
@@ -0,0 +1,13 @@
+package com.test.albo.mx.marvelchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiToAlboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiToAlboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/albo/mx/marvelchallenge/MarvelApiToAlboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.albo.mx.marvelchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiToAlboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
