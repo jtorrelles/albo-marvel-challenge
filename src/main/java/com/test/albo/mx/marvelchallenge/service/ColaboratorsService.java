a2f79349dbc978d2b106bebebcee5d4732ab8443
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-03 13:39:52 -0300

    se agrega el service layer

---
+++ src/main/java/com/test/albo/mx/marvelchallenge/service/ColaboratorsService.java
@@ -0,0 +1,7 @@
+package com.test.albo.mx.marvelchallenge.service;
+
+import com.test.albo.mx.marvelchallenge.dto.ColaboratorsDto;
+
+public interface ColaboratorsService {
+	ColaboratorsDto getColaborators(String characterName);
+}
---
+++ src/main/java/com/test/albo/mx/marvelchallenge/service/impl/ColaboratorsServiceImpl.java
@@ -0,0 +1,16 @@
+package com.test.albo.mx.marvelchallenge.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.test.albo.mx.marvelchallenge.dto.ColaboratorsDto;
+import com.test.albo.mx.marvelchallenge.service.ColaboratorsService;
+
+@Service
+public class ColaboratorsServiceImpl implements ColaboratorsService {
+
+	@Override
+	public ColaboratorsDto getColaborators(String characterName) {
+		return null;
+	}
+
+}
