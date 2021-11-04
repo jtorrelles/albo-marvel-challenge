8159fa036d2b8c98f140258694c34e69c3d53026
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-04 10:16:31 -0300

    se implementa funcionalidad en los servicios

--- src/main/java/com/test/albo/mx/marvelchallenge/service/impl/CharactersServiceImpl.java
+++ src/main/java/com/test/albo/mx/marvelchallenge/service/impl/CharactersServiceImpl.java
@@ -1,16 +1,57 @@
 package com.test.albo.mx.marvelchallenge.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.test.albo.mx.marvelchallenge.dto.CharactersDto;
+import com.test.albo.mx.marvelchallenge.dto.PartnersDto;
+import com.test.albo.mx.marvelchallenge.exception.GenericNotFoundException;
+import com.test.albo.mx.marvelchallenge.model.Comics;
+import com.test.albo.mx.marvelchallenge.repository.CharactersRepository;
 import com.test.albo.mx.marvelchallenge.service.CharactersService;
 
 @Service
 public class CharactersServiceImpl implements CharactersService {
 
+	private CharactersRepository charactersRepository;
+
+	public CharactersServiceImpl(CharactersRepository charactersRepository) {
+		this.charactersRepository = charactersRepository;
+	}
+
 	@Override
 	public CharactersDto getCharacters(String characterName) {
-		return null;
+		CharactersDto response = new CharactersDto();
+		var result = charactersRepository.findByShortName(characterName);
+		if (result.isPresent()) {
+
+			var character = result.get();
+			List<PartnersDto> partners = new ArrayList<>();
+
+			/**
+			 * Se ubican los partners (companeros) asociados al caracters (personaje) de la
+			 * busqueda y luego por cada partner se extraen los comics en comun
+			 */
+			character.getPartners().stream().forEach(partner -> {
+				var name = partner.getName();
+				var comics = partner.getComics().stream().distinct().map(Comics::getName).collect(Collectors.toList());
+
+				var partnerDto = new PartnersDto(name, comics);
+				if (!partners.contains(partnerDto)) {
+					partners.add(partnerDto);
+				}
+			});
+
+			response.setCharacters(partners);
+			response.setLastSync(character.getLastSync());
+
+		} else {
+			throw new GenericNotFoundException();
+		}
+		return response;
 	}
 
 }
--- src/main/java/com/test/albo/mx/marvelchallenge/service/impl/ColaboratorsServiceImpl.java
+++ src/main/java/com/test/albo/mx/marvelchallenge/service/impl/ColaboratorsServiceImpl.java
@@ -1,16 +1,65 @@
 package com.test.albo.mx.marvelchallenge.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.test.albo.mx.marvelchallenge.dto.ColaboratorsDto;
+import com.test.albo.mx.marvelchallenge.enums.ColaboratorRole;
+import com.test.albo.mx.marvelchallenge.exception.GenericNotFoundException;
+import com.test.albo.mx.marvelchallenge.repository.CharactersRepository;
 import com.test.albo.mx.marvelchallenge.service.ColaboratorsService;
 
 @Service
 public class ColaboratorsServiceImpl implements ColaboratorsService {
 
+	private CharactersRepository charactersRepository;
+
+	public ColaboratorsServiceImpl(CharactersRepository charactersRepository) {
+		this.charactersRepository = charactersRepository;
+	}
+
 	@Override
 	public ColaboratorsDto getColaborators(String characterName) {
-		return new ColaboratorsDto();
+		ColaboratorsDto response = new ColaboratorsDto();
+
+		var result = charactersRepository.findByShortName(characterName);
+
+		if (result.isPresent()) {
+			var character = result.get();
+
+			List<String> writers = new ArrayList<>();
+			List<String> colorists = new ArrayList<>();
+			List<String> editors = new ArrayList<>();
+
+			/**
+			 * Se obtienen los comics donde ha participado el caracter (heroe) y de ahi se
+			 * obtienen los distintos tipos de colaboradores
+			 */
+			character.getComics().stream().forEach(comic -> {
+				comic.getColaborators().stream().forEach(colaborator -> {
+					String name = colaborator.getName();
+					String role = colaborator.getRole();
+					if (role.equals(ColaboratorRole.COLORIST.name()) && !colorists.contains(name)) {
+						colorists.add(name);
+					} else if (role.equals(ColaboratorRole.WRITER.name()) && !writers.contains(name)) {
+						writers.add(name);
+					} else if (role.equals(ColaboratorRole.EDITOR.name()) && !editors.contains(name)) {
+						editors.add(name);
+					}
+				});
+			});
+
+			response.setLastSync(character.getLastSync());
+			response.setWriters(writers);
+			response.setColorists(colorists);
+			response.setEditors(editors);
+		} else {
+			throw new GenericNotFoundException();
+		}
+
+		return response;
 	}
 
 }
