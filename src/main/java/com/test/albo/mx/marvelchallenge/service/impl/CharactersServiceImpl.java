4786d939fce216ac7f9ecd0738518467f711b854
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-04 13:52:15 -0300

    se ajustan la implementacion de la capa de servicio

--- src/main/java/com/test/albo/mx/marvelchallenge/service/impl/CharactersServiceImpl.java
+++ src/main/java/com/test/albo/mx/marvelchallenge/service/impl/CharactersServiceImpl.java
@@ -1,15 +1,15 @@
 package com.test.albo.mx.marvelchallenge.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.test.albo.mx.marvelchallenge.dto.CharactersDto;
 import com.test.albo.mx.marvelchallenge.dto.PartnersDto;
 import com.test.albo.mx.marvelchallenge.exception.GenericNotFoundException;
-import com.test.albo.mx.marvelchallenge.model.Comics;
 import com.test.albo.mx.marvelchallenge.repository.CharactersRepository;
 import com.test.albo.mx.marvelchallenge.service.CharactersService;
 
@@ -29,20 +29,40 @@ public class CharactersServiceImpl implements CharactersService {
 		if (result.isPresent()) {
 
 			var character = result.get();
-			List<PartnersDto> partners = new ArrayList<>();
+			Map<String, List<String>> test = new HashMap<>();
 
 			/**
-			 * Se ubican los partners (companeros) asociados al caracters (personaje) de la
-			 * busqueda y luego por cada partner se extraen los comics en comun
+			 * Se ubican los comics del caracter (personaje) y luego por cada comic se
+			 * extraen los partnes (companeros) en comun, se crea un hashmap para utilizar
+			 * el companero como key del set de datos.
+			 * 
+			 * DATO: No se incluye dentro de los datos el caracter usado para la busqueda
 			 */
-			character.getPartners().stream().forEach(partner -> {
-				var name = partner.getName();
-				var comics = partner.getComics().stream().distinct().map(Comics::getName).collect(Collectors.toList());
+			character.getComics().stream().forEach(comic -> {
+				var comicName = comic.getName();
+				comic.getCharacters().stream().forEach(partner -> {
+					var name = partner.getFullName();
+					if (!name.equals(character.getFullName())) {
+						if (test.containsKey(name)) {
+							var comicsPartner = test.get(name);
+							if (!comicsPartner.contains(comicName)) {
+								comicsPartner.add(comicName);
+								test.replace(name, comicsPartner);
+							}
+						} else {
+							test.put(name, new ArrayList<String>() {
+								{
+									add(comicName);
+								}
+							});
+						}
+					}
+				});
+			});
 
-				var partnerDto = new PartnersDto(name, comics);
-				if (!partners.contains(partnerDto)) {
-					partners.add(partnerDto);
-				}
+			var partners = new ArrayList<PartnersDto>();
+			test.forEach((partner, comics) -> {
+				partners.add(new PartnersDto(partner, comics));
 			});
 
 			response.setCharacters(partners);
