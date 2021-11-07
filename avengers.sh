a1677ec8bbe47ffda44844f6b65fbf316f3cde23
jtorrelles <jhonnattan.torrelels@gmail.com>
2021-11-07 15:20:25 -0300

    se asignan las variables de los argumentos que ingrese el usuario

--- avengers.sh
+++ avengers.sh
@@ -3,4 +3,4 @@
 publicKey=$1
 privateKey=$2
 
-sudo java -jar -Dserver.port=80 -DEXTERNAL_PUBLIC_KEY=51f70bb07e8fae9bb1f9328532eca6f8 -DEXTERNAL_PRIVATE_KEY=dff9dc72b32645303726a4184a79c23a1e0469ee target/application.jar 
+sudo java -jar -Dserver.port=80 -DEXTERNAL_PUBLIC_KEY=$publicKey -DEXTERNAL_PRIVATE_KEY=$privateKey target/application.jar 
